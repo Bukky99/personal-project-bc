cc90070fe5dff21aa6dc42b4ab051cea4cdb9d2a
bukky <iloveherseys@gmail.com>
2021-02-18 13:49:59 +0000

    Removed console logs

--- server/routes/trainersRoutes.js
+++ server/routes/trainersRoutes.js
@@ -4,21 +4,18 @@ const Trainer = mongoose.model("trainers");
 module.exports = (app) => {
   app.get("/api/trainer", async (req, res) => {
     const query = req.query.q;
-    console.log("Q =", query);
     const result = await Trainer.find({ tags: {$all: query.split(/\s+/)}});
-    console.log(query.split(/\s+/))
-    console.log(result);
     res.json({
-      message: "Here are your searched sneakers...", //used for testing in postman
+      message: "Here are your searched sneakers...", //test in postman
       shoes: result,
     });
   });
 
   app.get("/api/randomTrainer", async (req, res) => {
     const randomSearch = await Trainer.aggregate([{$sample: {size:1}}]);
-    console.log(randomSearch)
+    //console.log(randomSearch)
     res.json({
-      message: "Your random sneaker", //used for testing in postman
+      message: "Your random sneaker", //test in postman
       shoe: randomSearch
     });
     
